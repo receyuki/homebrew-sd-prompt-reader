689d514417aef5dbeab4993499fc753aa3048f64
receyuki <receyuki@gmail.com>
2023-07-07 00:02:49 +0800

    Update sd-prompt-reader.rb

--- Casks/sd-prompt-reader.rb
+++ Casks/sd-prompt-reader.rb
@@ -2,8 +2,7 @@ cask "sd-prompt-reader" do
   version "1.3.1"
   sha256 "9370795bc04b246b4880a64560bd0fa74af9b52769d4c1a04e46bd4e0a450393"
 
-  url "https://github.com/receyuki/stable-diffusion-prompt-reader/releases/download/v#{version}/SD-Prompt-Reader-#{version}-macOS-universal.dmg",
-    verified: "github.com/receyuki/stable-diffusion-prompt-reader/"
+  url "https://github.com/receyuki/stable-diffusion-prompt-reader/releases/download/v#{version}/SD-Prompt-Reader-#{version}-macOS-universal.dmg"
   name "Stable Diffusion Prompt Reader"
   desc "A simple standalone viewer for reading prompt from Stable Diffusion generated image outside the webui."
   homepage "https://github.com/receyuki/stable-diffusion-prompt-reader"
