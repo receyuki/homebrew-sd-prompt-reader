60bb80d8432b4409b378d8fe868c5edac845a115
receyuki <receyuki@gmail.com>
2023-07-06 23:59:53 +0800

    Add cask

---
+++ Casks/sd-prompt-reader.rb
@@ -0,0 +1,22 @@
+cask "sd-prompt-reader" do
+  version "1.3.1"
+  sha256 "9370795bc04b246b4880a64560bd0fa74af9b52769d4c1a04e46bd4e0a450393"
+
+  url "https://github.com/receyuki/stable-diffusion-prompt-reader/releases/download/v#{version}/SD-Prompt-Reader-#{version}-macOS-universal.dmg",
+    verified: "github.com/receyuki/stable-diffusion-prompt-reader/"
+  name "Stable Diffusion Prompt Reader"
+  desc "A simple standalone viewer for reading prompt from Stable Diffusion generated image outside the webui."
+  homepage "https://github.com/receyuki/stable-diffusion-prompt-reader"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :el_capitan"
+
+  app "SD Prompt Reader.app"
+
+  uninstall quit: "com.receyuki.sd-prompt-reader"
+end
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# homebrew-sd-prompt-reader
+# Receyuki Sd-prompt-reader
+
+## How do I install these formulae?
+
+`brew install receyuki/sd-prompt-reader/<formula>`
+
+Or `brew tap receyuki/sd-prompt-reader` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
