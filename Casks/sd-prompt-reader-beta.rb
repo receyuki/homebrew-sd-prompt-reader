a141a955e7c57916928c7840eed0015045fcdf25
Rhys Yang <receyuki@gmail.com>
2023-07-26 21:43:42 +0800

    Update sd-prompt-reader-beta.rb

--- Casks/sd-prompt-reader-beta.rb
+++ Casks/sd-prompt-reader-beta.rb
@@ -1,6 +1,6 @@
 cask "sd-prompt-reader-beta" do
-  version "1.4.0b1.dev0"
-  sha256 "2fb780dd098b467fab4e3a6b06db39f339d31335a24350c5571c5a84aba10ee3"
+  version "1.4.0b1.dev1"
+  sha256 "f22caf12aeef615d2445453f23e57734599939752704ff83558e45234e11d152"
 
   url "https://receyuki.com/sdpr/SDPRbeta.zip"
   name "Stable Diffusion Prompt Reader Beta"
