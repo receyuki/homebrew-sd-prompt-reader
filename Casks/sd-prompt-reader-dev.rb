53c7c1c921fe1cb8a40829d0a9ba13f3537b0997
Rhys Yang <receyuki@gmail.com>
2023-09-02 00:08:17 +0800

    Update and rename sd-prompt-reader-beta.rb to sd-prompt-reader-dev.rb

--- Casks/sd-prompt-reader-dev.rb
+++ Casks/sd-prompt-reader-dev.rb
@@ -1,16 +1,16 @@
-cask "sd-prompt-reader-beta" do
+cask "sd-prompt-reader-dev" do
   version "1.3.3b2.dev1"
-  sha256 "c30e5cdbd7d934d930ebe9490eda8220a468b377b1cad2cd717cef9b95d57aca"
+  sha256 "21db3be50ca88a2fb2dbeb291de7d4f3500af6876eb6a46559a8ea1dbd6e4134"
 
-  url "https://receyuki.com/sdpr/SDPRbeta.zip"
-  name "Stable Diffusion Prompt Reader Beta"
+  url "https://receyuki.com/sdpr/SDPRdev.zip"
+  name "Stable Diffusion Prompt Reader Dev"
   desc "A simple standalone viewer for reading prompt from Stable Diffusion generated image outside the webui."
   homepage "https://github.com/receyuki/stable-diffusion-prompt-reader"
 
   auto_updates true
   depends_on macos: ">= :el_capitan"
 
-  app "SD Prompt Reader Beta.app"
+  app "SD Prompt Reader Dev.app"
 
   uninstall quit: "com.receyuki.sd-prompt-reader"
 end
