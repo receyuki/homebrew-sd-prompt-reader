4a761e39ac899c47d223137fd9caaeccae36e526
Rhys Yang <receyuki@gmail.com>
2023-10-26 01:09:10 +0800

    Update sd-prompt-reader-dev.rb

--- Casks/sd-prompt-reader-dev.rb
+++ Casks/sd-prompt-reader-dev.rb
@@ -1,6 +1,6 @@
 cask "sd-prompt-reader-dev" do
-  version "1.3.4b2"
-  sha256 "1f0c1d4c4359bcb3de3b4319d8ee84676f98b7bac7241edc9b2fa8e19a9dfef6"
+  version "1.3.4"
+  sha256 "e997ba09205b25cfd9e910d6cf33b6eb0eb0af64ad737213d66abf85863dce0d"
 
   url "https://receyuki.com/sdpr/SDPRdev.zip"
   name "Stable Diffusion Prompt Reader Dev"
