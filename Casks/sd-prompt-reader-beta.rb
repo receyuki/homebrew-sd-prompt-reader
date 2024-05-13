59a7225bbf168ab559728ce94ef4dbf54056f109
Rhys Yang <receyuki@gmail.com>
2024-05-13 20:29:13 +0800

    Update sd-prompt-reader-beta.rb

--- Casks/sd-prompt-reader-beta.rb
+++ Casks/sd-prompt-reader-beta.rb
@@ -1,6 +1,6 @@
 cask "sd-prompt-reader-beta" do
-  version "1.3.5-beta.1"
-  sha256 "c7a95a0349bbbaf18be13677ce3afef52bf3abe873a09b7b156de5d8583ecc1c"
+  version "1.3.5-beta.2"
+  sha256 "8bc31dc0ed3d3bc04c299159d6a3e46f32a6581be522aa2905c590dddfe16438"
 
   url "https://github.com/receyuki/stable-diffusion-prompt-reader/releases/download/v#{version}/SD-Prompt-Reader-#{version}-macOS-universal.dmg"
   name "Stable Diffusion Prompt Reader"
